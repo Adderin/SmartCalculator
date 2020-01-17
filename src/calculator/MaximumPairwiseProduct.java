97281fd8f95f325f6a0d446933e2831a00fb30d1
Adderin <adderin@gmail.com>
2020-01-17 22:53:07 +0100

    hyperskill.org - The maximum pairwise product

---
+++ src/calculator/MaximumPairwiseProduct.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class MaximumPairwiseProduct {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[scanner.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+    }
+}
