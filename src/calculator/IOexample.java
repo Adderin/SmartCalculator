fe4760c9ef300591cfdad4173387bcd3e9b53d87
Adderin <adderin@gmail.com>
2020-01-18 23:42:18 +0100

    hyperskill.org - Smart Calculator

--- src/calculator/IOexample.java
+++ src/calculator/IOexample.java
@@ -6,16 +6,19 @@ public class IOexample {
     public static void main(String[] args) {
         do {
             Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            if (input.equals("/exit")) {
-                System.out.println("Bye");
-                break;
-            }
+            try {
+                String input = scanner.nextLine();
+                if (input.equals("/exit")) {
+                    System.out.println("Bye!");
+                    break;
+                }
             String[] str = input.split(" ");
             if (str.length <= 1) {
                 System.out.println(Integer.parseInt(str[0]));
             } else {
                 System.out.println(Integer.parseInt(str[0]) + Integer.parseInt(str[1]));
+            }
+            } catch (Exception e) {
             }
         } while(true);
     }
