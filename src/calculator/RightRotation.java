e3f1e0fd4439b3edc6e2b28fa1b6821ea465456a
Adderin <adderin@gmail.com>
2020-01-18 20:17:10 +0100

    hyperskill.org - Right Rotation

--- src/calculator/RightRotation.java
+++ src/calculator/RightRotation.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 public class RightRotation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] arr = new int[5];
-        for (int i = 0; i < 5; i++) {
-            arr[i] = scanner.nextInt();
+        String str = scanner.nextLine();
+        String[] strings = str.split(" ");
+        int[] arr = new int[strings.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(strings[i]);
         }
         int shift = scanner.nextInt();
         shift = shift % arr.length;
-        int[] arr_copy = new int[arr.length];
+        int[] arrCopy = new int[arr.length];
         for (int j = 0; j < arr.length; j++) {
             if (j + shift <= arr.length - 1) {
-                arr_copy[j + shift] = arr[j];
+                arrCopy[j + shift] = arr[j];
             } else {
-                arr_copy[j + shift - arr.length] = arr[j];
+                arrCopy[j + shift - arr.length] = arr[j];
             }
         }
-        for (int i : arr_copy)
+        for (int i : arrCopy)
             System.out.print(i + " ");
     }
 }
